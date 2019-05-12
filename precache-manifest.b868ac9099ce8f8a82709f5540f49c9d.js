9c33a6d4c1b6fe1288b014612fce1d06e2c3b9a4
ali <46577578+alinaeemaei@users.noreply.github.com>
2019-05-12 22:04:41 +0430

    Updates

---
+++ precache-manifest.b868ac9099ce8f8a82709f5540f49c9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b82fc03fa6c28e1ff020fba346779cc",
+    "url": "//github.com/alinaeemaei/Hello/index.html"
+  },
+  {
+    "revision": "524d0db26967ee37ce44",
+    "url": "//github.com/alinaeemaei/Hello/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "//github.com/alinaeemaei/Hello/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "524d0db26967ee37ce44",
+    "url": "//github.com/alinaeemaei/Hello/static/js/main.b4ba663c.chunk.js"
+  },
+  {
+    "revision": "e2ce7f7f1f4975117073",
+    "url": "//github.com/alinaeemaei/Hello/static/js/runtime~main.854f730f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//github.com/alinaeemaei/Hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
